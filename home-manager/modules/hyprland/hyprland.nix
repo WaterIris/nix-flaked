545a0bc901176f8ebb162e50d63268af71e7ed6d
WaterIris <179727984+WaterIris@users.noreply.github.com>
2025-03-09 21:43:59 +0100

    fix

--- home-manager/modules/hyprland/hyprland.nix
+++ home-manager/modules/hyprland/hyprland.nix
@@ -52,6 +52,11 @@
 
         "opacity 0.90,focus:1,class:kitty"
         "opacity 0.70,focus:0,class:kitty"
+
+        "workspace 1, class:kitty"
+        "workspace 2, class:firefox"
+        "workspace 3, class:obsidian"
+        "workspace 8, class:spotify"
       ];
 
       "monitor" = [
