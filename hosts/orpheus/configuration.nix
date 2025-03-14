238f4cf9c2c7af674313a002438b83f5852748ff
WaterIris <179727984+WaterIris@users.noreply.github.com>
2025-03-14 04:31:12 +0100

    add battery conservation

--- hosts/orpheus/configuration.nix
+++ hosts/orpheus/configuration.nix
@@ -53,6 +53,16 @@
     variant = "";
   };
 
+  services.logind.lidSwitch = "ignore";
+
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      START_CHARGE_THRESH_BAT0 = 0; # Dummy value
+      STOP_CHARGE_THRESH_BAT0 = 1; # Actualy enables conservation mode
+      };
+};
   # services.displayManager.sddm = {
     # enable = true;
     # wayland = {
